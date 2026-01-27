d692a5f5151f53576e7658bbaad6b74f97b7b20e
oluwafemi <oluwafemidojumo@gmail.com>
2026-01-27 11:17:13 +0100

    firebase config

--- dating_api_project/dating/apps.py
+++ dating_api_project/dating/apps.py
@@ -4,5 +4,3 @@ from django.apps import AppConfig
 class DatingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dating'
-    from .firebase_utils import init_firebase
-    init_firebase()
