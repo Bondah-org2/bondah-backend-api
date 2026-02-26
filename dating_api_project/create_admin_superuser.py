b7cb9cec8e61b03c9b1a82e8e1ab63e7057b99ee
oluwafemi <oluwafemidojumo@gmail.com>
2026-02-26 18:33:13 +0100

    Lock Python to 3.11

--- dating_api_project/create_admin_superuser.py
+++ dating_api_project/create_admin_superuser.py
@@ -7,9 +7,9 @@ django.setup()
 
 User = get_user_model()
 
-USERNAME = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-EMAIL = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-PASSWORD = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin12345")
+USERNAME = os.environ.get("DJANGO_SUPERUSER_USERNAME", "new")
+EMAIL = os.environ.get("DJANGO_SUPERUSER_EMAIL", "new@example.com")
+PASSWORD = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin1234")
 
 if not User.objects.filter(username=USERNAME).exists():
     print("Creating superuser...")
