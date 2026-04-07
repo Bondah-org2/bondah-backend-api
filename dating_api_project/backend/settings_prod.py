b15b7bdb0f745e7213389ab252b8382d404e6f92
oluwafemi <oluwafemidojumo@gmail.com>
2026-04-07 07:41:15 +0100

    admin updated

--- dating_api_project/backend/settings_prod.py
+++ dating_api_project/backend/settings_prod.py
@@ -282,6 +282,11 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 20,
     # API Documentation
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+    ],
 }
 
 # REST Auth settings
